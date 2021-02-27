bead7367c27bae7939e1ea8b958f9bfc68ab881b
mochi-MizLab <h20ms419@hirosaki-u.ac.jp>
2021-02-27 21:31:09 +0900

    Update: add db

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from .db import TodoAppDbClient
 
 app = Flask(__name__)
+client = TodoAppDbClient()
 
 
 @app.route("/")
 def index():
     return "index page"
+
+
+@app.route("/", methods=["POST"])
+def store_data():
+    data = request.json
+    client.insert({"title": data["title"], "id": data["id"]})
+    response = jsonify()
+    return response, 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
