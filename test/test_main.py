2087cc511c2677137f6837cd54d2b2f6798eeecb
mochi-MizLab <h20ms419@hirosaki-u.ac.jp>
2021-02-27 21:40:15 +0900

    red: add new test case

--- test/test_main.py
+++ test/test_main.py
@@ -10,8 +10,14 @@ class TestTodoAPI(BaseTestCase):
 
     def test_post_todo(self):
         post_param = {"title": "go to school", "id": 1}
-
         response = self.app.post("/",
                                  data=json.dumps(post_param),
                                  content_type="application/json")
         self.assert_status(response, 200)
+
+    def test_post_some_query(self):
+        post_param = {"title": "go to school", "id": 1}    # same one
+        response = self.app.post("/",
+                                 data=json.dumps(post_param),
+                                 content_type="application/json")
+        self.assert_status(response, 409)
